b2e69179de10896c20289f2e2a3979c2eddc694d
ricalva <ricardo.am2000@gmail.com>
2022-05-31 12:28:28 -0500

    ejercicio 1y2

---
+++ meses.js
@@ -0,0 +1,15 @@
+var mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// Se muestra el nombre de cada mes
+alert(mes[0]);
+alert(mes[1]);
+alert(mes[2]);
+alert(mes[3]);
+alert(mes[4]);
+alert(mes[5]);
+alert(mes[6]);
+alert(mes[7]);
+alert(mes[8]);
+alert(mes[9]);
+alert(mes[10]);
+alert(mes[11]);
